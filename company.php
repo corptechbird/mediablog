66af9188265d402e285ffe7727edbba37b38337d
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-07-27 20:41:38 +0900

    Add bizmedia page

--- company.php
+++ company.php
@@ -16,10 +16,10 @@ Template Name: Company
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <!-- Favicon, Thumbnail image -->
-    <link rel="icon" type="image/x-icon" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/images/favicon-16.ico">
-    <link rel="icon" type="image/x-icon" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/images/favicon-32.ico">
-    <link rel="icon" type="image/x-icon" sizes="48x48" href="<?php bloginfo('template_directory'); ?>/images/favicon-48.ico">
-    <link rel="icon" type="image/x-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/images/favicon-152.ico">
+    <link rel="icon" type="image/x-icon" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/images/biz_fav-16.ico">
+    <link rel="icon" type="image/x-icon" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/images/biz_fav-32.ico">
+    <link rel="icon" type="image/x-icon" sizes="48x48" href="<?php bloginfo('template_directory'); ?>/images/biz_fav-48.ico">
+    <link rel="icon" type="image/x-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/images/biz_fav-152.ico">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -28,6 +28,35 @@ Template Name: Company
 </head>
 <body>
     <script>
+        $(window).on('load',function(){
+            // fade-in
+            $(window).scroll(function (){
+                $('.fade-in').each(function(){
+                    var POS = $(this).offset().top;  //fade-inがついている要素の位置
+                    var scroll = $(window).scrollTop();  //スクロール一
+                    var windowHeight = $(window).height();  //ウィンドウの高さ
+
+                    if (scroll > POS - windowHeight + windowHeight*2/5){
+                        $(this).css("opacity","1" );
+                    } else {
+                        $(this).css("opacity","0" );
+                    }
+                });
+            });
+        });
+
+        $(function(){
+            $('').click(function() {
+                var speed = 400;
+                var href= $(this).attr("href");
+                var target = $(href == "#" || href == "" ? 'html' : href);
+                var headerHeight = 50; //固定ヘッダーの高さ
+                var position = target.offset().top - headerHeight; //ターゲットの座標からヘッダの高さ分引く
+                $('body,html').animate({scrollTop:position}, speed, 'swing');
+                return false;
+            });
+        });
+
         var click_count = 0;    // クリック数の初期化
         function menu_click(){
             if( click_count % 2 == 0 ){
@@ -37,10 +66,10 @@ Template Name: Company
                 $('#menu').css('padding','20px');
                 $('#menu ul li').css('text-align','center');
                 $('#menu').css('width','100%');
-                document.getElementById("btn").innerHTML='<a href="javascript:menu_click();"><i class="fas fa-times fa-lg"></i></a>';
+                document.getElementById("bbtn").innerHTML='<a href="javascript:menu_click();"><i class="fas fa-times fa-lg"></i></a>';
             }else{
                 $('#menu').css('display','none');
-                document.getElementById("btn").innerHTML='<a href="javascript:menu_click();"><i class="fas fa-bars fa-lg"></i></a>';
+                document.getElementById("bbtn").innerHTML='<a href="javascript:menu_click();"><i class="fas fa-bars fa-lg"></i></a>';
 
 //                 var $win = $(window);
 
@@ -59,22 +88,18 @@ Template Name: Company
             click_count++;
         }
     </script>
-    <div id="pageTop"><a href="#"><i class="fa fa-chevron-up"></i></a></div>
         <div id="wrapper">
-            <!-- Header -->       
+            <!-- Header -->
             <div id="header">
-            
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>/bizmedia">
                 <img src="<?php bloginfo('template_directory'); ?>/images/company.png"> </a>
-                <span id="navi">
-                <span id="btn"><a href="javascript:menu_click();">MENU</a></span>
+                <span id="bbtn"><a href="javascript:menu_click();"><i class="fas fa-bars fa-lg"></i></a></span>
                 <span id="menu">
                     <ul>
-                        <li class="navi"><a href="#mission">MISSION</a></li>
-                        <li class="navi"><a href="#vision">VISION</a></li>
-                        <li class="navi"><a href="#business">BUSINESS</a></li>
-                        <li class="navi"><a href="#contact">CONTACT</a></li>
-                        <li class="close"><a href="javascript:menu_click();">×</a></li>
+                        <li class="bnavi"><a href="#mission">MISSION</a></li>
+                        <li class="bnavi"><a href="#vision">VISION</a></li>
+                        <li class="bnavi"><a href="#business">BUSINESS</a></li>
+                        <li class="bnavi"><a href="#contact">CONTACT</a></li>
                     </ul>
                 </span><!-- #menu -->
             </div>      
@@ -90,32 +115,46 @@ Template Name: Company
                 }
             </style>
 <!-- Contents -->             
-            <div id="contents">
-                <div id="fullpage">  
-                    <div id="mission">
-                        <p>MISSION</p>
-                        <p>Webマーケティングを世界一簡単にする</p>
-                        <p>手を動かしながら自分たちで良いものを作り出していくことがミッションです。ベーシック独自のWebマーケティング力を軸に展開する様々な事業で、社会のあらゆるものを問題解決することがベーシックのミッションです。</p>
+            <div id="frame-contents">
+                <div id="mission" class="fade-in">
+                    <h4>MISSION</h4>
+                    <div class="phrase-form">
+                        <span><h5>ITを通じて社会に貢献する</h5>
+                        日々進化していくITを手を動かしながら検証し、
+                        毎日の生活の問題・課題を改善するソリューションを自ら作りだすことがミッションです。
+                        長年の研究開発で培った技術力を軸に展開する様々な事業で、
+                        ユーザーからのフィードバックを大切にしながら本当に必要とされるプロダクトを提供していきます。</span>
                     </div>
-                    <div id="vision">
-                        <p>VISION</p>
-                        <p>Webマーケティングをもっと身近に</p>
-                        <p>世の中をもっとクリエイティブに</p>
-                        <p>より多くの人にWebマーケティングが広まれば、社会はもっとクリエイティブになる。私たちのビジョンである「Webマーケティングの大衆化」を実現することで、社会の問題解決を目指す。それがベーシックです。</p>
+                </div>
+                <div id="vision" class="fade-in">
+                    <h4>VISION</h4>
+                    <div class="phrase-form">
+                        <span><h5>最先端のITを活用できるプラットフォームを提供する</h5>
+                        最新のITを使うことにより今までになかった新しい世界を創ることができます。
+                        我々は世界に目を向けながら最先端のITの研究開発・ソリューション開発を実施し、
+                        その成果を人々が容易に活用できる環境・プラットフォームを提供していきます。
+                        そして、日常生活や社会の問題・課題解決を目指します。bizMediaの思いはそこにあります。</span>
                     </div>
-                    <div id="business">
-                        <p>BUSINESS</p>
+                </div>
+                <div id="business" class="fade-in">
+                    <h4>BUSINESS</h4>
+                    <div class="phrase-form">
+                        <span><h5>3つの重点事業</h5>
+                        bizMediaでは、①モバイル/Webアプリ開発、②メディア運営、③プログラミング教育の3つの事業を軸に活動しています。</br>
+                        ①モバイル/Webアプリ開発では、AIやAR/VRを組み込んだ業務アプリや2D/3Dゲームアプリをメインで開発しています。</br>
+                        ②メディア運営では、<a href="http://techbird.site" target="_blank">TECHBIRD</a>というメディアにて海外ITトレンドを発信しています。</br>
+                        ③プログラミング教育では、Swift, JavaScript, PHP, Java, Pythonを中心に教えています。
                     </div>
-                    モバイル/Webアプリ開発
-
-                    メディア運営
-
-                    プログラミング教育
-                    <div id="contact">
-                        <p>CONTACT</p>
-                        <?php echo do_shortcode('[contact-form-7 id="248" title="コンタクトフォーム 1"]'); ?>
+                </div>
+                
+                <div id="contact" class="fade-in">
+                    <h4>CONTACT</h4>
+                    <div class="phrase-form">
+                    <span>
+                    <?php echo do_shortcode('[contact-form-7 id="248" title="コンタクトフォーム 1"]'); ?>
+            </span>
                     </div>
-                </div>  
+                </div>
             </div><!-- /#contents -->
             <!-- Footer -->             
             <div id="footer">
@@ -123,16 +162,9 @@ Template Name: Company
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
                     <img src="<?php bloginfo('template_directory'); ?>/images/company.png"> </a>
                 </div>
-                <div class="text">
-                    <p class="category">Category</p>
-                    <ul><?php wp_list_categories('title_li=&sort_column=name&hide_empty=1&hierarchical=1&style=list'); ?></ul>
-                </div>
-                <div class="copy">
-                    &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> All rights reserved.
-                </div>
             </div><!-- /#footer -->
         </div><!-- /#wrapper -->
-    </div><!-- /#pageTop --> 
+
 <?php wp_footer(); ?>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b58a0fda9fc44fd"></script>
 <script>
