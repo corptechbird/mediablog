e93f0fc3bd550b1b498b0851645c28d32df8c8b2
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-08-04 00:12:16 +0900

    Update footer design

--- single.php
+++ single.php
@@ -25,37 +25,37 @@
                 'post__not_in' => array( $post->ID ),
                 'category__in' => $catkwds,
                 'orderby' => 'rand'
-            ) ); 
+            )); 
             if( $myposts ): ?>
-            <div><i class="fas fa-bookmark" aria-hidden="true"></i> 他の人はこちらの記事も読んでいます</div>
-            <div class="cards">
-                <?php foreach($myposts as $post):
-                setup_postdata($post); ?>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
-                    <div class="l-card">
-                        <div class="l-thumbnail">
-                            <figure class="thumbnail-wrapper">
-                            <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
-                                the_post_thumbnail(array(360, 360));
-                                } else { //アイキャッチ画像を設定していない場合 ?>
-                                <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
-                            <?php } ?>
-                            </figure>
-                            <span class="more-text">Read More</span>
-                        </div>
-                        <div class="text-content">
-                            <p class="caption"><?php the_title(); ?></p>
-                            <div class="content-meta">
-                                <span class="date"><i class="far fa-clock" aria-hidden="true"></i> <?php the_date(); ?> <?php the_time(); ?></span>
+                <div><i class="fas fa-bookmark" aria-hidden="true"></i> 他の人はこちらの記事も読んでいます</div>
+                <div class="cards">
+                    <?php foreach($myposts as $post):
+                    setup_postdata($post); ?>
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
+                        <div class="l-card">
+                            <div class="l-thumbnail">
+                                <figure class="thumbnail-wrapper">
+                                <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
+                                    the_post_thumbnail(array(360, 360));
+                                    } else { //アイキャッチ画像を設定していない場合 ?>
+                                    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
+                                <?php } ?>
+                                </figure>
+                                <span class="more-text">Read More</span>
+                            </div>
+                            <div class="text-content">
+                                <p class="caption"><?php the_title(); ?></p>
+                                <div class="content-meta">
+                                    <span class="date"><i class="far fa-clock" aria-hidden="true"></i> <?php the_date(); ?> <?php the_time(); ?></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <?php endforeach; ?>
+                    </a>
+                    <?php endforeach; ?>
                 </div>
-            </div>
-            <?php wp_reset_postdata();
+                <?php wp_reset_postdata();
             endif; ?>
+            </div>
             <?php endwhile;    // 繰り返し処理終了 ?>
             <?php else :    // 投稿がない場合 ?>
             <?php endif;    // 条件分岐終了 ?>
