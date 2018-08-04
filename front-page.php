5726fe21233742b9ec9b8fa3a11c5070aeb8d3ed
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-08-04 22:31:19 +0900

    Ajust cards line

--- front-page.php
+++ front-page.php
@@ -5,28 +5,30 @@
             <?php if ( have_posts() ) :    // もし投稿があるなら ?>
                 <div class="newentry">New Entries</div>
                 <div class="cards">
-                <?php while ( have_posts() ) : the_post();    // 繰り返し処理（ループ）開始 ?>
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
-                        <div class="l-card">
-                            <div class="l-thumbnail">
-                                <figure class="thumbnail-wrapper">
-                                <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
-                                    the_post_thumbnail(array(360, 360));
-                                    } else { //アイキャッチ画像を設定していない場合 ?>
-                                    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
-                                <?php } ?>
-                                </figure>
-                                <span class="more-text">Read More</span>
-                            </div>
-                            <div class="text-content">
-                                <p class="caption"><?php the_title(); ?></p>
-                                <div class="content-meta">
-                                    <span class="date"><span class="far fa-clock" aria-hidden="true"></span> <?php the_date(); ?> <?php the_time(); ?></span>
+                    <div class="cards-inside">
+                        <?php while ( have_posts() ) : the_post();    // 繰り返し処理（ループ）開始 ?>
+                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
+                                <div class="l-card">
+                                    <div class="l-thumbnail">
+                                        <figure class="thumbnail-wrapper">
+                                        <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
+                                            the_post_thumbnail(array(360, 360));
+                                            } else { //アイキャッチ画像を設定していない場合 ?>
+                                            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
+                                        <?php } ?>
+                                        </figure>
+                                        <span class="more-text">Read More</span>
+                                    </div>
+                                    <div class="text-content">
+                                        <p class="caption"><?php the_title(); ?></p>
+                                        <div class="content-meta">
+                                            <span class="date"><span class="far fa-clock" aria-hidden="true"></span> <?php the_date(); ?> <?php the_time(); ?></span>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </a>
-                <?php endwhile;    // 繰り返し処理終了 ?>
+                            </a>
+                        <?php endwhile;    // 繰り返し処理終了 ?>
+                    </div>
                 </div>
                 <p class="pagenation">
                     <?php
