981d7ec93f152e81463aa24feeef684caba59f29
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-09-03 19:24:08 +0900

    Update sigle ref

--- single.php
+++ single.php
@@ -5,7 +5,10 @@
             <div class="post-main">
                 <?php if ( have_posts() ) :    // もし投稿があるなら ?>
                 <?php while ( have_posts() ) : the_post();    // 繰り返し処理（ループ）開始 ?>
-                    <figure class="frame"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/python/"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/python.png" alt="アイキャッチ画像" width="360"></a></figure>
+                <?php
+                    $post_type = get_post_type();
+                    ?>
+                    <figure class="frame"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $post_type ?>/"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $post_type ?>.png" alt="<?php echo $post_type ?>画像" width="360"></a></figure>
                     <p class="title"><?php the_title(); ?></p>
                     <div class="tag"><?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?></div>
                     <?php wp_link_pages('before=<p id="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
@@ -60,7 +63,7 @@
                         <?php $array = array("swift", "python", "javascript", "ruby");
                         foreach($array as $value){ //fruitsの先頭から１つずつ$fruitに代入する
                             ?>
-                            <a href="<?php echo get_bloginfo('template_directory'); ?>/<?php echo $value; ?>" title="逆引き<?php echo $value; ?>">  
+                            <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $value; ?>" title="逆引き<?php echo $value; ?>">  
                                 <div class="l-card">
                                     <div class="l-thumbnail">
                                         <figure class="thumbnail-wrapper">
