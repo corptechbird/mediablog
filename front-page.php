91cd4576bc448f2fbda2ac3b014be3182b9dd938
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-09-03 19:44:11 +0900

    Update text

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
                 <div class="newentry"></div>
                 <div class="cards">
                     <div class="cards-inside">
-                        <?php $array = array("Swift", "Python", "JavaScript", "Ruby");
+                        <?php $array = array("swift", "python", "javascript", "ruby");
                         foreach($array as $value){ //fruitsの先頭から１つずつ$fruitに代入する
                             ?>
                             <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $value; ?>" title="逆引き<?php echo $value; ?>">  
@@ -31,48 +31,6 @@
             <div class="notfound">
                 <h3>ページが見つかりません</h3>
                 <p>申し訳ありません。お探しのページは見つかりませんでした。</p>
-                <div class="notfound-navi"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fas fa-home" aria-hidden="true"></i> Home へ戻る</a></div>
-                <div class="cards">
-                    <div class="cards-inside">
-                <?php 
-                    query_posts('showposts=20');
-                    if (have_posts()) : 
-                        ?>
-                        <div class="cards">
-                        <?php 
-                        while (have_posts()) : 
-                        the_post();
-                        ?>
-                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
-                            <div class="l-card">
-                                <div class="l-thumbnail">
-                                <figure class="thumbnail-wrapper">
-                                <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
-                                    the_post_thumbnail(array(360, 360));
-                                    } else { //アイキャッチ画像を設定していない場合 ?>
-                                    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
-                                <?php } ?>
-                                </figure>
-                                    <span class="more-text">Read More</span>
-                                </div>
-                                <div class="text-content">
-                                    <p class="caption"><?php the_title(); ?></p>
-                                    <div class="content-meta">
-                                        <span class="date">
-                                        <span class="far fa-clock" aria-hidden="true"></span> <?php the_time('Y.m.d') ?> <?php the_time(); ?></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a> 
-                        <?php
-                        endwhile;
-                        ?>
-                        </div>
-                        <?php 
-                    endif;
-                ?>
-                    </div>
-                </div>
             </div>
             <?php endif;    // 条件分岐終了 ?>
             <div class="newentry"></div>
