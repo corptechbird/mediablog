55b21fbbf260c036a227ddfc98161de47c166da7
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-09-03 15:54:34 +0900

    Update single page

--- page-python.php
+++ page-python.php
@@ -1,8 +1,33 @@
 <?php get_header(); ?>
 <!-- Contents -->             
     <div id="contents">
-        <div class="post">   
-        <div class="newentry">『逆引きPython』記事一覧</div>     
+        <div class="post">
+        <?php $post_name = ""; 
+            $slug_name = basename(get_permalink());
+            if ($slug_name == 'python') {
+                $post_name = "Python";
+            } elseif ($slug_name == 'swift') {
+                $post_name = "Swift";
+            } elseif ($slug_name == 'javascript') {
+                $post_name = "JavaScript";
+            } elseif ($slug_name == 'ruby') {
+                $post_name = "Ruby";
+            }
+        ?>
+        <div class="newentry">『逆引き<?php echo $post_name; ?>』記事一覧</div>     
+                <?php
+            $wp_query = new WP_Query();
+            $param = array(
+                'posts_per_page' => '-1', //表示件数を''に入力。-1なら全件表示になります
+                'post_type' => $slug_name, //カスタム投稿タイプのスラグ（名称）を入れる
+                'post_status' => 'publish', //取得する記事の状態。publishなら一般公開のもののみ
+                'orderby' => 'ID', //ID順に並び替えをする
+                'order' => 'ASC' //IDの大きい順（新しい記事順）に上から表示する
+            );
+            $wp_query->query($param);
+            if($wp_query->have_posts()): while($wp_query->have_posts()) : $wp_query->the_post();
+        ?>
+        <?php endwhile; endif; ?>
             <?php if ( have_posts() ) :    // もし投稿があるなら ?>
                 <div class="cards">
                     <div class="cards-inside">
@@ -11,7 +36,7 @@
                                 <div class="l-card">
                                     <div class="l-thumbnail">
                                         <figure class="thumbnail-wrapper">
-                                            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/python.png" alt="アイキャッチ画像" width="360" height="220" />
+                                            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $slug_name; ?>.png" alt="アイキャッチ画像" width="360" height="220" />
                                         </figure>
                                         <span class="more-text">Read More</span>
                                     </div>
@@ -40,51 +65,8 @@
             <div class="notfound">
                 <h3>ページが見つかりません</h3>
                 <p>申し訳ありません。お探しのページは見つかりませんでした。</p>
-                <div class="notfound-navi"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fas fa-home" aria-hidden="true"></i> Home へ戻る</a></div>
-                <div class="cards">
-                    <div class="cards-inside">
-                <?php 
-                    query_posts('showposts=20');
-                    if (have_posts()) : 
-                        ?>
-                        <div class="cards">
-                        <?php 
-                        while (have_posts()) : 
-                        the_post();
-                        ?>
-                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
-                            <div class="l-card">
-                                <div class="l-thumbnail">
-                                <figure class="thumbnail-wrapper">
-                                <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
-                                    the_post_thumbnail(array(360, 360));
-                                    } else { //アイキャッチ画像を設定していない場合 ?>
-                                    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
-                                <?php } ?>
-                                </figure>
-                                    <span class="more-text">Read More</span>
-                                </div>
-                                <div class="text-content">
-                                    <p class="caption"><?php the_title(); ?></p>
-                                    <div class="content-meta">
-                                        <span class="date">
-                                        <span class="far fa-clock" aria-hidden="true"></span> <?php the_time('Y.m.d') ?> <?php the_time(); ?></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a> 
-                        <?php
-                        endwhile;
-                        ?>
-                        </div>
-                        <?php 
-                    endif;
-                ?>
-                </div>
-                </div>
             </div>
             <?php endif;    // 条件分岐終了 ?>
-
             <div class="newentry"></div>
             <div class="ads" style="margin-bottom:20px;">
                         <div class="adleft">
--- single.php
+++ single.php
@@ -3,30 +3,50 @@
     <div id="contents">
         <div class="post">
             <div class="post-main">
-                <?php if(function_exists("the_breadcrumb")){the_breadcrumb();} ?>
                 <?php if ( have_posts() ) :    // もし投稿があるなら ?>
                 <?php while ( have_posts() ) : the_post();    // 繰り返し処理（ループ）開始 ?>
+                    <figure class="frame"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/python/"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/python.png" alt="アイキャッチ画像" width="360"></a></figure>
                     <p class="title"><?php the_title(); ?></p>
                     <div class="tag"><?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?></div>
-                    <span class="time"><i class="far fa-clock" aria-hidden="true"></i> <?php the_date(); ?> <?php the_time(); ?></span>
-                    <figure class="frame"><?php the_post_thumbnail(); ?></figure>
                     <?php wp_link_pages('before=<p id="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
-                    <?php the_content(); ?>
+                    <p>Python3</p>
                     <div class="ads" style="margin-bottom:20px;">
                         <div class="adleft">
                             <ins class="adsbygoogle"
-                                style="display:inline-block;width:320px;height:100px"
+                                style="display:inline-block;width:336px;height:280px"
                                 data-ad-client="ca-pub-2330122305834701"
-                                data-ad-slot="4335446353"></ins>
+                                data-ad-slot="4283281946"></ins>
                             <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                             </script>
                         </div>
                         <div class="adright">
                             <ins class="adsbygoogle"
-                                style="display:inline-block;width:320px;height:100px"
+                                style="display:inline-block;width:336px;height:280px"
                                 data-ad-client="ca-pub-2330122305834701"
-                                data-ad-slot="4335446353"></ins>
+                                data-ad-slot="5240483851"></ins>
+                            <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                        <hr class="clear">
+                    </div>
+                    <?php the_content(); ?>
+                    <div class="ads" style="margin-bottom:20px;">
+                        <div class="adleft">
+                            <ins class="adsbygoogle"
+                                style="display:inline-block;width:336px;height:280px"
+                                data-ad-client="ca-pub-2330122305834701"
+                                data-ad-slot="4283281946"></ins>
+                            <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                        <div class="adright">
+                            <ins class="adsbygoogle"
+                                style="display:inline-block;width:336px;height:280px"
+                                data-ad-client="ca-pub-2330122305834701"
+                                data-ad-slot="5240483851"></ins>
                             <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                             </script>
@@ -36,55 +56,31 @@
                 <?php wp_link_pages('before=<p id="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
             </div>
             <div class="afterpost">
-            <?php if( has_category() ) {
-                $cats = get_the_category();
-                $catkwds = array();
-                foreach($cats as $cat) {
-                    $catkwds[] = $cat->term_id;
-                }
-            } ?>
-            <?php
-            $myposts = get_posts( array(
-                'post_type' => 'post',
-                'posts_per_page' => '9',
-                'post__not_in' => array( $post->ID ),
-                'category__in' => $catkwds,
-                'orderby' => 'rand'
-            )); 
-            if( $myposts ): ?>
-                <div><i class="fas fa-bookmark" aria-hidden="true"></i> 他の人はこちらの記事も読んでいます</div>
                 <div class="cards">
-                <div class="cards-inside">
-                    <?php foreach($myposts as $post):
-                    setup_postdata($post); ?>
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">  
-                        <div class="l-card">
-                            <div class="l-thumbnail">
-                                <figure class="thumbnail-wrapper">
-                                <?php if (has_post_thumbnail()) { //アイキャッチ画像を設定している場合
-                                    the_post_thumbnail(array(360, 360));
-                                    } else { //アイキャッチ画像を設定していない場合 ?>
-                                    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="アイキャッチ画像" width="360" height="240" />
-                                <?php } ?>
-                                </figure>
-                                <span class="more-text">Read More</span>
-                            </div>
-                            <div class="text-content">
-                                <p class="caption"><?php the_title(); ?></p>
-                                <div class="content-meta">
-                                    <span class="date"><i class="far fa-clock" aria-hidden="true"></i> <?php the_time('Y.m.d') ?> <?php the_time(); ?></span>
+                    <div class="cards-inside">
+                        <?php $array = array("swift", "python", "javascript", "ruby");
+                        foreach($array as $value){ //fruitsの先頭から１つずつ$fruitに代入する
+                            ?>
+                            <a href="<?php echo get_bloginfo('template_directory'); ?>/<?php echo $value; ?>" title="逆引き<?php echo $value; ?>">  
+                                <div class="l-card">
+                                    <div class="l-thumbnail">
+                                        <figure class="thumbnail-wrapper">
+                                            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $value; ?>.png" alt="アイキャッチ画像" width="360" height="220" />
+                                        </figure>
+                                        <span class="more-text">Read More</span>
+                                    </div>
+                                    <div class="text-content">
+                                        <p class="caption"><?php echo $value; ?></p>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </a>
-                    <?php endforeach; ?>
+                            </a>
+                            <?php
+                        } ?>
                     </div>
                 </div>
-                <?php wp_reset_postdata();
-            endif; ?>
+                <?php wp_reset_postdata(); ?>
             </div>
             <?php endwhile;    // 繰り返し処理終了 ?>
-            <?php else :    // 投稿がない場合 ?>
             <?php endif;    // 条件分岐終了 ?>
         </div>
     </div>
