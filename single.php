d6597552d9cc71ca40c3718a0b730b6953a8ee73
Shoki Takeda <shokitakeda@Shokis-MacBook-Air.local>
2018-09-03 17:56:26 +0900

    Update ads

--- single.php
+++ single.php
@@ -11,19 +11,19 @@
                     <?php wp_link_pages('before=<p id="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
                     <div class="ads" style="margin-bottom:20px;">
                         <div class="adleft">
-                            <ins class="adsbygoogle"
-                                style="display:inline-block;width:336px;height:280px"
-                                data-ad-client="ca-pub-2330122305834701"
-                                data-ad-slot="4283281946"></ins>
-                            <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
+                        <ins class="adsbygoogle"
+                            style="display:inline-block;width:320px;height:100px"
+                            data-ad-client="ca-pub-2330122305834701"
+                            data-ad-slot="4335446353"></ins>
+                        <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                         </div>
                         <div class="adright">
                             <ins class="adsbygoogle"
-                                style="display:inline-block;width:336px;height:280px"
+                                style="display:inline-block;width:320px;height:100px"
                                 data-ad-client="ca-pub-2330122305834701"
-                                data-ad-slot="5240483851"></ins>
+                                data-ad-slot="4335446353"></ins>
                             <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                             </script>
