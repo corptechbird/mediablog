94b0d78ed3308f6774ab44fd5e71af17982a641c
corptechbird <corp.techbird@gmail.com>
2018-10-25 04:57:45 +0900

    Update English document

--- single.php
+++ single.php
@@ -7,22 +7,14 @@
             <?php
                 $post_type = get_post_type();
                 $post_name = ""; 
-                if ($post_type == 'python') {
+                if ($slug_name == 'python') {
                     $post_name = "Python";
-                } elseif ($post_type == 'flutter') {
-                    $post_name = "Flutter";
-                } elseif ($post_type == 'go') {
-                    $post_name = "Go";
-                } elseif ($post_type == 'swift') {
+                } elseif ($slug_name == 'swift') {
                     $post_name = "Swift";
-                } elseif ($post_type == 'kotlin') {
+                } elseif ($slug_name == 'kotlin') {
                     $post_name = "Kotlin";
-                } elseif ($post_type == 'android') {
+                } elseif ($slug_name == 'android') {
                     $post_name = "Android";
-                } elseif ($post_type == 'scala') {
-                    $post_name = "Scala";
-                } elseif ($post_type == 'unity') {
-                    $post_name = "Unity";
                 }
                 ?>
                 <figure class="frame">
@@ -63,7 +55,7 @@
                 </div>
                 <?php wp_link_pages('before=<p id="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
             </div>
-            <div class="newentry">『逆引き<?php echo $post_name; ?>』記事一覧</div>     
+            <div class="newentry">『<?php echo $post_name; ?> Dictionary』Article List</div>     
             <ul>
                 <?php $args = array(
                     'numberposts' => -1,                //表示（取得）する記事の数
@@ -83,38 +75,30 @@
             <div class="afterpost">
                 <div class="cards">
                     <div class="cards-inside">
-                        <?php $array = array("swift", "python", "go", "scala", "android", "kotlin", "flutter", "unity");
+                        <?php $array = array("swift", "python", "android", "kotlin");
                         foreach($array as $value){
                         ?>
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $value; ?>" title="逆引き<?php echo $value; ?>">  
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $value; ?>" title="<?php echo $value; ?> Dictionary">  
                             <div class="l-card">
                                 <div class="l-thumbnail">
                                     <figure class="thumbnail-wrapper">
-                                        <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $value; ?>.png" alt="アイキャッチ画像" width="360" height="220" />
+                                        <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $value; ?>.png" alt="Eye Catch Image" width="360" height="220" />
                                     </figure>
                                     <span class="more-text">Read More</span>
                                 </div>
                                 <div class="text-content">
                                     <?php 
-                                        if ($value == 'python') {
-                                            $value = "Python";
-                                        } elseif ($value == 'flutter') {
-                                            $value = "Flutter";
-                                        } elseif ($value == 'go') {
-                                            $value = "Go";
-                                        } elseif ($value == 'swift') {
-                                            $value = "Swift";
-                                        } elseif ($value == 'kotlin') {
-                                            $value = "Kotlin";
-                                        } elseif ($value == 'android') {
-                                            $value = "Android";
-                                        } elseif ($value == 'scala') {
-                                            $value = "Scala";
-                                        } elseif ($value == 'unity') {
-                                            $value = "Unity";
+                                        if ($slug_name == 'python') {
+                                            $post_name = "Python";
+                                        } elseif ($slug_name == 'swift') {
+                                            $post_name = "Swift";
+                                        } elseif ($slug_name == 'kotlin') {
+                                            $post_name = "Kotlin";
+                                        } elseif ($slug_name == 'android') {
+                                            $post_name = "Android";
                                         }
                                     ?>
-                                    <p class="caption"><?php echo $value; ?>でできることからプログラムを逆引きできるドキュメントです。実行結果の画像を添付していますし、プログラムは動作確認済みなのでコピペでもすぐ動きます。</p>
+                                    <p class="caption">This document shows the program of <?php echo $value; ?>, images and videos of the execution results at a glance. This is suitable for beginners to use as reference.</p>
                                 </div>
                             </div>
                         </a>
