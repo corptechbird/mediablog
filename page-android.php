035c24435a803fa47d91040e3f4d8b155c85a38a
corptechbird <corp.techbird@gmail.com>
2018-10-25 23:09:05 +0900

    Update template

--- page-android.php
+++ page-android.php
@@ -13,7 +13,6 @@
                 $post_name = "Android";
             }
         ?>
-            <div class="newentry">『<?php echo $post_name; ?> Dictionary』Article List</div>     
             <?php
             $wp_query = new WP_Query();
             $param = array(
