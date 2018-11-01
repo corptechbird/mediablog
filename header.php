09e33b5701818a7e7f126868a48441a766790a3f
corptechbird <corp.techbird@gmail.com>
2018-11-01 23:56:50 +0900

    Update href

--- header.php
+++ header.php
@@ -58,14 +58,15 @@
 </head>
 <body>
     <script>
-        $(function () {
-            var headerHight = 50; //ヘッダの高さ
-            $('a[href^=#]').click(function(){
+        $(function(){
+            $('a[href^=#]').click(function() {
+                var speed = 400;
                 var href= $(this).attr("href");
                 var target = $(href == "#" || href == "" ? 'html' : href);
-                    var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
-                $("html, body").animate({scrollTop:position}, 550, "swing");
-                    return false;
+                var headerHeight = 55; //固定ヘッダーの高さ
+                var position = target.offset().top - headerHeight; //ターゲットの座標からヘッダの高さ分引く
+                $('body,html').animate({scrollTop:position}, speed, 'swing');
+                return false;
             });
         });
         var addthis_config = addthis_config||{};
