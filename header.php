02435334c08b1f9743129d531281572ed9b87d5d
corptechbird <corp.techbird@gmail.com>
2018-11-01 23:51:07 +0900

    Update href

--- header.php
+++ header.php
@@ -58,6 +58,23 @@
 </head>
 <body>
     <script>
+        $(window).on('load',function(){
+            // fade-in
+            $(window).scroll(function (){
+                $('.fade-in').each(function(){
+                    var POS = $(this).offset().top;  //fade-inがついている要素の位置
+                    var scroll = $(window).scrollTop();  //スクロール一
+                    var windowHeight = $(window).height();  //ウィンドウの高さ
+
+                    if (scroll > POS - windowHeight + windowHeight*2/5){
+                        $(this).css("opacity","1" );
+                    } else {
+                        $(this).css("opacity","0" );
+                    }
+                });
+            });
+        });
+        
         $(function(){
             $('').click(function() {
                 var speed = 400;
