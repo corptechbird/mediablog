32dafaa9dbd900f08271989cccbc106a2bb075f5
corptechbird <corp.techbird@gmail.com>
2018-11-02 00:11:03 +0900

    Update href

--- footer.php
+++ footer.php
@@ -66,18 +66,6 @@
                 return false;
             });
         });
-        $(function(){
-            $('').click(function() {
-                var speed = 4100;
-                var href= $(this).attr("href");
-                var target = $(href == "#" || href == "" ? 'html' : href);
-                var headerHeight = 100; //固定ヘッダーの高さ
-                var position = target.offset().top - headerHeight; //ターゲットの座標からヘッダの高さ分引く
-                $('body,html').animate({scrollTop:position}, speed, 'swing');
-                console.log('動いてるで');
-                return false;
-            });
-        }); 
     </script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -58,6 +58,34 @@
 </head>
 <body>
     <script>
+        $(window).on('load',function(){
+            // fade-in
+            $(window).scroll(function (){
+                $('.fade-in').each(function(){
+                    var POS = $(this).offset().top;  //fade-inがついている要素の位置
+                    var scroll = $(window).scrollTop();  //スクロール一
+                    var windowHeight = $(window).height();  //ウィンドウの高さ
+
+                    if (scroll > POS - windowHeight + windowHeight*2/5){
+                        $(this).css("opacity","1" );
+                    } else {
+                        $(this).css("opacity","0" );
+                    }
+                });
+            });
+        });
+        $(function(){
+            $('').click(function() {
+                var speed = 400;
+                var href= $(this).attr("href");
+                var target = $(href == "#" || href == "" ? 'html' : href);
+                var headerHeight = 52; //固定ヘッダーの高さ
+                var position = target.offset().top - headerHeight; //ターゲットの座標からヘッダの高さ分引く
+                $('body,html').animate({scrollTop:position}, speed, 'swing');
+                console.log('動いてるで');
+                return false;
+            });
+        }); 
         var addthis_config = addthis_config||{};
         addthis_config.data_track_addressbar = false;
         addthis_config.data_track_clickback = false;
