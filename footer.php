9fe111f7ddb7798e3f1ee86fd2e35b223ff4dfb9
corptechbird <corp.techbird@gmail.com>
2018-11-10 20:54:31 +0900

    Update header

--- footer.php
+++ footer.php
@@ -74,7 +74,7 @@
                 var speed = 400;
                 var href= $(this).attr("href");
                 var target = $(href == "#" || href == "" ? 'html' : href);
-                var headerHeight = 48; //固定ヘッダーの高さ
+                var headerHeight = 55; //固定ヘッダーの高さ
                 var position = target.offset().top - headerHeight; //ターゲットの座標からヘッダの高さ分引く
                 $('body,html').animate({scrollTop:position}, speed, 'swing');
                 return false;
