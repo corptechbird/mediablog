aefa72b354575c7c8044f695b2524cefc9a28cf6
corptechbird <corp.techbird@gmail.com>
2018-11-25 16:53:59 +0900

    Update

--- category.php
+++ category.php
@@ -27,7 +27,7 @@
             $param = array(
                 'posts_per_page' => '-1', 
                 'post_type' => 'post', 
-                'category' => esc_html( $cat_info->slug ),
+                'category_name' => esc_html( $cat_info->slug ),
                 'post_status' => 'publish', 
                 'orderby' => 'ID', 
                 'order' => 'DESC' 
@@ -41,7 +41,9 @@
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $post_type ?>/">
                 <?php if (has_post_thumbnail()) {
                         the_post_thumbnail(array(360, 220));
-                    } elseif (get_post_type() == 'datascience' || get_post_type() == 'datascience_cn' || get_post_type() == 'python' || get_post_type() == 'python_cn' ||  get_post_type() == 'swift' ||  get_post_type() == 'swift_cn' ) { ?>
+                    } elseif (get_post_type() == 'post') { ?>
+                        <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo esc_html( $cat_info->slug ); ?>.png" alt="<?php echo $post_name; ?> Image" width="360" />
+                    <?php } elseif (get_post_type() == 'datascience' || get_post_type() == 'datascience_cn' || get_post_type() == 'python' || get_post_type() == 'python_cn' ||  get_post_type() == 'swift' ||  get_post_type() == 'swift_cn' ) { ?>
                         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $post_type; ?>.png" alt="<?php echo $post_type; ?> Image" width="360" />
                     <?php } else { ?>
                         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/eyecatch.png" alt="Eye Catch Image" width="360" />
