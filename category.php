620b24b6406df42875b7f30006482b2924e86a28
corptechbird <corp.techbird@gmail.com>
2018-11-25 21:40:16 +0900

    Update

--- category.php
+++ category.php
@@ -82,7 +82,7 @@
                 <div class="cards">
                     <div class="cards-inside">
                     <?php
-                    $taxonomies = array('post');
+                    $taxonomies = array('python');
                     //Set arguments - don't 'hide' empty terms.
                     $args = array(
                         'hide_empty' => 0
