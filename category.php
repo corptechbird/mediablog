7b6eedbbefd4a4b56e6b006df73cc7f6e7a350d9
corptechbird <corp.techbird@gmail.com>
2018-11-25 19:04:25 +0900

    Update

--- category.php
+++ category.php
@@ -39,9 +39,7 @@
             <?php if ( have_posts() ) : ?>
             <figure class="frame">
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $post_type ?>/">
-                <?php if (has_post_thumbnail()) {
-                        the_post_thumbnail(array(360, 220));
-                    } elseif (get_post_type() == 'post') { ?>
+                <?php if (get_post_type() == 'post') { ?>
                         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo esc_html( $cat_info->slug ); ?>.png" alt="<?php echo $post_name; ?> Image" width="360" />
                     <?php } elseif (get_post_type() == 'datascience' || get_post_type() == 'datascience_cn' || get_post_type() == 'python' || get_post_type() == 'python_cn' ||  get_post_type() == 'swift' ||  get_post_type() == 'swift_cn' ) { ?>
                         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $post_type; ?>.png" alt="<?php echo $post_type; ?> Image" width="360" />
