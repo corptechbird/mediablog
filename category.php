a1c841d539477f5c3a9551377bd0eff2f525a278
corptechbird <corp.techbird@gmail.com>
2018-11-25 19:30:02 +0900

    Update

--- category.php
+++ category.php
@@ -105,37 +105,30 @@
             <div class="afterpost">
                 <div class="cards">
                     <div class="cards-inside">
-                    <?php $array = array("datascience", "datascience_cn", "python", "python_cn", "swift", "swift_cn");
-                        foreach($array as $value){
-                        $value_name = ""; 
-                        if ($value == 'datascience') {
-                            $value_name = "Data Science";
-                        } elseif ($value == 'datascience_cn') {
-                            $value_name = "Data Science";
-                        } elseif ($value == 'python') {
-                            $value_name = "Python";
-                        } elseif ($value == 'python_cn') {
-                            $value_name = "Python";
-                        } elseif ($value == 'swift') {
-                            $value_name = "Swift";
-                        } elseif ($value == 'swift_cn') {
-                            $value_name = "Swift";
-                        }
-                        ?>
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $value; ?>" title="<?php echo $value; ?> Reference">  
-                            <div class="l-card">
-                                <div class="l-thumbnail">
-                                    <figure class="thumbnail-wrapper">
-                                        <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $value; ?>.png" alt="Eye Catch Image" width="360" height="220" />
-                                    </figure>
-                                    <span class="more-text">Read More</span>
-                                </div>
-                                <div class="text-content">
-                                    <p class="caption">This document shows the program of <?php echo $value_name; ?>, images and videos of the execution results at a glance. This is suitable for beginners to use as reference.</p>
-                                </div>
+                    <?php 
+                    $args = array(
+                        'type'                     => 'post',
+                        'orderby'                  => 'name',
+                        'order'                    => 'ASC'
+                    );
+                    $categories = get_categories( $args );
+                    
+                    foreach($categories as $value){
+                    ?>
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>/<?php echo $value; ?>" title="<?php echo $value; ?> Reference">  
+                        <div class="l-card">
+                            <div class="l-thumbnail">
+                                <figure class="thumbnail-wrapper">
+                                    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/<?php echo $value; ?>.png" alt="Eye Catch Image" width="360" height="220" />
+                                </figure>
+                                <span class="more-text">Read More</span>
                             </div>
-                        </a>
-                        <?php
+                            <div class="text-content">
+                                <p class="caption">This document shows the program of <?php echo $value; ?>, images and videos of the execution results at a glance. This is suitable for beginners to use as reference.</p>
+                            </div>
+                        </div>
+                    </a>
+                    <?php
                     } ?>
                     <a href="http://techbird.site:8080/tools" title="Data Science Automator">  
                         <div class="l-card">
