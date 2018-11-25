5ed4687b2735dc94c9cb5d5c814c6a6e0e397c23
corptechbird <corp.techbird@gmail.com>
2018-11-25 19:07:47 +0900

    Update

--- category.php
+++ category.php
@@ -27,7 +27,7 @@
             $param = array(
                 'posts_per_page' => '-1', 
                 'post_type' => 'post', 
-                'category_name' => esc_html( $cat_info->name ),
+                'category_name' => esc_html( $cat_info->slug ),
                 'post_status' => 'publish', 
                 'orderby' => 'ID', 
                 'order' => 'DESC' 
