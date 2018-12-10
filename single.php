b56579c63f8fb1eb9d0820784b85a109ea3b7328
corptechbird <corp.techbird@gmail.com>
2018-12-11 00:25:53 +0900

    Update

--- single.php
+++ single.php
@@ -20,6 +20,13 @@
                 <?php } ?>
                 </a>
             </figure>
+            <ins class="adsbygoogle"
+                style="display:inline-block;width:728px;height:90px"
+                data-ad-client="ca-pub-2330122305834701"
+                data-ad-slot="3318403188"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
             <div class="title"><?php the_title(); ?></div>
             <div class="tag"><?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?></div>
             <?php wp_link_pages('before=<p id="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
