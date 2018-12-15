8a5ff86cea731c5a2058e6d14d52ae1005c3ad26
corptechbird <corp.techbird@gmail.com>
2018-12-15 17:03:25 +0900

    Update

--- contact.php
+++ contact.php
@@ -14,14 +14,22 @@ Template Name: Contact
     line-height: 1em;
     }
 </style>
-<!-- Contents -->             
-    <div id="contents">
-        <div class="post">
-            <div class="newentry">CONTACT</div>
-            <div class="cards">
-                <?php echo do_shortcode('[contact-form-7 id="257" title="contact"]'); ?>
-            </div>
-            <div class="newentry"></div>
+<!-- Contents -->
+<form action="https://goo.gl/forms/L2UdtnIhYLytxY0n1">
+  <label for="email">メールアドレス</label>
+  <input id="email" type="text" name="entry.1045781291" placeholder="example@mail.co.jp">
+  <label for="msg">お問い合わせ内容</label>
+  <textarea id="msg" name="entry.839337160" placeholder="お気軽にお問い合わせください。"></textarea>
+  <button type="submit" name="button" value="送信"></button>
+</form>
+       
+<div id="contents">
+    <div class="post">
+        <div class="newentry">CONTACT</div>
+        <div class="cards">
+            <?php echo do_shortcode('[contact-form-7 id="257" title="contact"]'); ?>
         </div>
-    </div><!-- /#main -->
+        <div class="newentry"></div>
+    </div>
+</div><!-- /#main -->
 <?php get_footer(); ?>
