c09973322aad87ba90c39e6ce86152325522fe1b
corptechbird <corp.techbird@gmail.com>
2018-12-15 17:05:57 +0900

    Update

--- contact.php
+++ contact.php
@@ -15,16 +15,15 @@ Template Name: Contact
     }
 </style>
 <!-- Contents -->
-<form action="https://goo.gl/forms/L2UdtnIhYLytxY0n1">
-  <label for="email">メールアドレス</label>
-  <input id="email" type="text" name="entry.1045781291" placeholder="example@mail.co.jp">
-  <label for="msg">お問い合わせ内容</label>
-  <textarea id="msg" name="entry.839337160" placeholder="お気軽にお問い合わせください。"></textarea>
-  <button type="submit" name="button" value="送信"></button>
-</form>
-       
 <div id="contents">
     <div class="post">
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLScESylDuJPQCz5-mq0dnVB7Qzkl9ttU9z7yRtRNijBCgcXSYg/viewform?usp=sf_link">
+    <label for="email">メールアドレス</label>
+    <input id="email" type="text" name="entry.1045781291" placeholder="example@mail.co.jp">
+    <label for="msg">お問い合わせ内容</label>
+    <textarea id="msg" name="entry.839337160" placeholder="お気軽にお問い合わせください。"></textarea>
+    <button type="submit" name="button" value="送信"></button>
+    </form>
         <div class="newentry">CONTACT</div>
         <div class="cards">
             <?php echo do_shortcode('[contact-form-7 id="257" title="contact"]'); ?>
