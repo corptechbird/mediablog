4a57ab6f8c58bdc58bc639358a69d2326e1d2ac2
corptechbird <corp.techbird@gmail.com>
2019-02-14 10:57:01 +0900

    U

--- company.php
+++ company.php
@@ -11,7 +11,6 @@ Template Name: Company
     <meta name="description" content="Contribute to society by the latest IT">
     <meta name="keywords" content="Reference, Programming, Coding, Sample, Example, Tutorial, Download, Beginner, Entrance, Study">
 <!-- External files -->
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/theOstrichClub/MediaBlog/master/style.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
@@ -909,7 +908,7 @@ a.next.page-numbers:hover{
   background-color: rgba(251, 251, 251, 1);
 }
 #mission {
-  background-image : url(images/mission.png);
+  background-image : url(https://raw.githubusercontent.com/theOstrichClub/MediaBlog/master/images/mission.png);
   background-repeat : no-repeat;
   background-position : center center;  
   background-size:cover;
