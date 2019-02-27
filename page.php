05352c0039d0561c273c6f44c5455b6caa4ea396
corptechbird <corp.techbird@gmail.com>
2019-02-28 07:45:51 +0900

    U

--- page.php
+++ page.php
@@ -63,17 +63,37 @@
             <ul style="list-style-type:decimal; padding-left: 40px;">
             <?php $counter = 0; ?>  
             <?php while ( have_posts() ) : the_post(); ?>
-            <?php if($counter>0 && $counter%20==0):?>
+            <?php if($counter>0 && $counter%100==0):?>
             <div class="ads" style="margin-bottom:20px;">
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-2330122305834701"
-                    data-ad-slot="9848871375"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                <div class="adleft">
+                    <ins class="adsbygoogle"
+                        style="display:inline-block;width:300px;height:600px"
+                        data-ad-client="ca-pub-2330122305834701"
+                        data-ad-slot="8660278409"></ins>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+                <div class="adright">
+                    <ins class="adsbygoogle"
+                        style="display:inline-block;width:300px;height:600px"
+                        data-ad-client="ca-pub-2330122305834701"
+                        data-ad-slot="8660278409"></ins>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+                <hr class="clear">
+            </div>
+            <?php elseif($counter>0 && $counter%20==0):?>
+            <div class="ads" style="margin-bottom:20px;">
+            <ins class="adsbygoogle"
+                style="display:inline-block;width:728px;height:90px"
+                data-ad-client="ca-pub-2330122305834701"
+                data-ad-slot="3318403188"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
             </div>
             <?php endif; ?>
             <li>
