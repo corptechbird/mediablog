68866c1c8553d412ee566ad2beb338798a125fa3
corptechbird <corp.techbird@gmail.com>
2019-02-28 07:36:42 +0900

    u

--- category.php
+++ category.php
@@ -51,16 +51,39 @@
             <ul style="list-style-type:decimal; padding-left: 40px;">
             <?php $counter = 0; ?>  
             <?php while ( have_posts() ) : the_post(); ?>
-            <?php if($counter>0 && $counter%20==0):?>
+            <?php if($counter>0 && $counter%100==0):?>
+            <div class="ads" style="margin-bottom:20px;">
+                <div class="adleft">
+                    <ins class="adsbygoogle"
+                        style="display:inline-block;width:300px;height:600px"
+                        data-ad-client="ca-pub-2330122305834701"
+                        data-ad-slot="8660278409"></ins>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+                <div class="adright">
+                    <ins class="adsbygoogle"
+                        style="display:inline-block;width:300px;height:600px"
+                        data-ad-client="ca-pub-2330122305834701"
+                        data-ad-slot="8660278409"></ins>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+                <hr class="clear">
+            </div>
+            <?php elseif($counter>0 && $counter%20==0):?>
             <div class="ads" style="margin-bottom:20px;">
             <ins class="adsbygoogle"
-                style="display:inline-block;width:728px;height:90px"
+                style="display:block"
                 data-ad-client="ca-pub-2330122305834701"
-                data-ad-slot="3318403188"></ins>
+                data-ad-slot="9848871375"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
             <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
-            </div>
             <?php endif; ?>
             <li>
                 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
