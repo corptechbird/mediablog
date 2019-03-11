fbd4591616faf608cd07284f425268078e1d0a39
corptechbird <corp.techbird@gmail.com>
2019-03-11 13:38:23 +0900

    U

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <span><form role="search" method="get" id="searchform" action="/" autocomplete="off" >
-<input type="text" value="" name="s" placeholder="Search TECHBIRD" class="s" />
+<input type="text" value="" name="s" placeholder="Search BlueGate" class="s" />
 <input type="submit" class="searchsubmit" value="Search" />
 </form></span>
