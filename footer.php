d28ed69b5c0af1d971219dd950a227a3f2d1c9d2
corptechbird <corp.techbird@gmail.com>
2019-03-14 14:21:12 +0900

    u

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
                 <div>
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
                     <!-- <img src="<?php bloginfo('template_directory'); ?>/images/icon.png"> </a> -->
-                    <img src="https://raw.githubusercontent.com/theOstrichClub/MediaBlog/master/images/icon.png"> </a>
+                    <img src="https://raw.githubusercontent.com/corptechbird/mediablog/master/images/icon.png"> </a>
                 </div>
                 <div class="text">
                     <!-- <p class="category">Category</p>
