c45825b183bd38c81d5de6fa015b539854643e9a
corptechbird <corp.techbird@gmail.com>
2019-04-04 16:53:32 +0900

    u

--- single.php
+++ single.php
@@ -41,7 +41,7 @@
             <?php
             $cat = get_the_category();
             $cat = $cat[0];?>
-            <div><a href="<?php echo esc_url( home_url( '/' ) ); ?>/category/<?php echo $cat->slug; ?>" title="<?php echo $cat->name; ?>"><i class="fas fa-newspaper"></i> All Articles of Music Trend in <?php echo $term->name; ?></div>
+            <div><a href="<?php echo esc_url( home_url( '/' ) ); ?>/category/<?php echo $cat->slug; ?>" title="<?php echo $cat->name; ?>"><i class="fas fa-newspaper"></i> All Articles of Music Trend in <?php echo $cat->name; ?></div>
             <?php the_content(); ?>
             <?php
                 $prev_post = get_previous_post();
