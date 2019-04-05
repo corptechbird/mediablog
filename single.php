ea32578fbfb5b76d44e70ab64eb198031579b0cd
corptechbird <corp.techbird@gmail.com>
2019-04-05 16:34:48 +0900

    u

--- single.php
+++ single.php
@@ -91,14 +91,7 @@
                 'order' => 'DESC' 
             );
             $posts = get_posts( $args );
-            if( $posts ) : foreach( $posts as $post) : setup_postdata( $post ); ?>
-            <li style="padding-left:10px; border-bottom: 1px solid #d7d7d7; display: flex; flex-wrap: wrap;"> 
-                <div style="width: 100px; margin: 0;"><?php the_post_thumbnail(array(100, 80, true)); ?></div>
-                <div style="padding-left:8px;width:650px;"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></div>
-            </li> 
-            <?php endforeach; ?>
-            <?php else : ?>
-            <?php endif; ?>
+            ?>
         </ul>
         <div class="afterpost">
             <div class="ads" style="margin-bottom:20px;">
